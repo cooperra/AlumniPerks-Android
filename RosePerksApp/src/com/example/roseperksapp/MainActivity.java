69ac94770393b06ca93f39858b192768952152aa
Robbie Cooper <cooperra@rose-hulman.edu>
2014-01-06 18:23:48 -0500

    Add an action listener to the login button

--- RosePerksApp/src/com/example/roseperksapp/MainActivity.java
+++ RosePerksApp/src/com/example/roseperksapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.roseperksapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +18,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void onLoginButtonClick(View button) {
+		// TODO: login here
+		//button.setEnabled(false);
 	}
 
 }
