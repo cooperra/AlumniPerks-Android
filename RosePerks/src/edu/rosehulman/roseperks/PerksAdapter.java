55f31a8f002c1d95a31de43548a77ce43ecabf6b
Thomas Morris <morrista@rose-hulman.edu>
2014-01-29 17:03:54 -0500

    Work in progress

--- RosePerks/src/edu/rosehulman/roseperks/PerksAdapter.java
+++ RosePerks/src/edu/rosehulman/roseperks/PerksAdapter.java
@@ -5,8 +5,11 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,11 +17,11 @@ import android.widget.TextView;
 public class PerksAdapter extends BaseAdapter {
 	
 	private Activity activity;
-	private ArrayList<HashMap<String, String>> data;
+	private List<HashMap<String, String>> data;
 	private static LayoutInflater inflater = null;
 	public ImageLoader imageLoader;
 	
-	public PerksAdapter(Activity a, ArrayList<HashMap<String, String>> d){
+	public PerksAdapter(Activity a, List<HashMap<String, String>> d){
 		activity = a;
 		data = d;
 		inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -41,6 +44,7 @@ public class PerksAdapter extends BaseAdapter {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
+		//Setting up the view
 		View vi = convertView;
 		if(convertView == null){
 			vi = inflater.inflate(R.layout.activity_perks_list, null);
@@ -55,6 +59,15 @@ public class PerksAdapter extends BaseAdapter {
 		company = data.get(position);
 		
 		name.setText(company.get(PerksListView.KEY_NAME));
+		location.setText(company.get(PerksListView.KEY_LOCATION));
+		number.setText(company.get(PerksListView.KEY_NUMBER));
+		discount.setText(company.get(PerksListView.KEY_DISCOUNT));
+		String uri = "drawable/"+ data.get(position).get(PerksListView.KEY_NAME_IMAGE);
+		//Making the image
+		int imageResource = vi.getContext().getApplicationContext().getResources().getIdentifier(
+		   uri, null, vi.getContext().getApplicationContext().getPackageName());
+		Drawable image = vi.getContext().getResources().getDrawable(imageResource);
+		name_image.setImageDrawable(image);
 		return vi;
 	}
 
--- RosePerks/src/edu/rosehulman/roseperks/PerksListView.java
+++ RosePerks/src/edu/rosehulman/roseperks/PerksListView.java
@@ -1,10 +1,21 @@
 package edu.rosehulman.roseperks;
 import android.app.Activity;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,7 +24,7 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 public class PerksListView extends Activity {
-	static final String URL = "http://companylist.xml";
+//	static final String URL = "companylist.xml";
 	static final String KEY_NAME = "name";
 	static final String KEY_ID = "id";
 	static final String KEY_LOCATION = "location";
@@ -28,29 +39,75 @@ public class PerksListView extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perks_list_items);
+        DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = null;
+		try {
+			docBuilder = docBuilderFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        Document doc = null;
+		try {
+			doc = docBuilder.parse (getAssets().open("companyList.xml"));
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         
-        ArrayList<HashMap<String, String>> companyList = new ArrayList<HashMap<String, String>>();
-        XMLParser parser = new XMLParser();
-        String xml = parser.getXmlFromUrl(URL);
-        Document doc = parser.getDomElement(xml);
+        List<HashMap<String,String>> perksListCollection = new ArrayList<HashMap<String,String>>();
         
-        NodeList n1 = doc.getElementsByTagName(KEY_NAME);
-        for (int i = 0 ; i < n1.getLength(); i++){
+        doc.getDocumentElement().normalize();
+        
+        NodeList perksList = doc.getElementsByTagName("companyData");
+        
+        for (int i = 0 ; i < perksList.getLength(); i++){
         	HashMap<String, String> map = new HashMap<String, String>();
-        	Element e = (Element) n1.item(i);
-        	map.put(KEY_ID, parser.getValue(e, KEY_ID));
-        	map.put(KEY_NAME, parser.getValue(e, KEY_NAME));
-        	map.put(KEY_LOCATION, parser.getValue(e, KEY_LOCATION));
-        	map.put(KEY_NUMBER, parser.getValue(e, KEY_NUMBER));
-        	map.put(KEY_DISCOUNT, parser.getValue(e, KEY_DISCOUNT));
-        	map.put(KEY_NAME_IMAGE, parser.getValue(e, KEY_NAME_IMAGE));
         	
-        	companyList.add(map);
+        	Node firstPerksNode = perksList.item(i);
+        	
+        	if (firstPerksNode.getNodeType() == Node.ELEMENT_NODE){
+        		Element firstPerksElement = (Element)firstPerksNode;
+        		NodeList idList = firstPerksElement.getElementsByTagName(KEY_ID);
+        		Element firstIdElement = (Element)idList.item(0);
+        		NodeList textIdList = firstIdElement.getChildNodes();
+        		map.put(KEY_ID, ((Node)textIdList.item(0)).getNodeValue().trim());
+        		
+        		NodeList nameList = firstPerksElement.getElementsByTagName(KEY_NAME);
+        		Element firstNameElement = (Element)nameList.item(0);
+        		NodeList textNameList = firstNameElement.getChildNodes();
+        		map.put(KEY_NAME, ((Node)textNameList.item(0)).getNodeValue().trim());
+        		
+        		NodeList locationList = firstPerksElement.getElementsByTagName(KEY_LOCATION);
+        		Element firstLocationElement = (Element)locationList.item(0);
+        		NodeList textLocationList = firstLocationElement.getChildNodes();
+        		map.put(KEY_LOCATION, ((Node)textLocationList.item(0)).getNodeValue().trim());
+        		
+        		NodeList numberList = firstPerksElement.getElementsByTagName(KEY_NUMBER);
+        		Element firstNumberElement = (Element)numberList.item(0);
+        		NodeList textNumberList = firstNumberElement.getChildNodes();
+        		map.put(KEY_NUMBER, ((Node)textNumberList.item(0)).getNodeValue().trim());
+        		
+        		NodeList discountList = firstPerksElement.getElementsByTagName(KEY_DISCOUNT );
+        		Element firstDiscountElement = (Element)discountList.item(0);
+        		NodeList textDiscountList = firstDiscountElement.getChildNodes();
+        		map.put(KEY_DISCOUNT, ((Node)textDiscountList.item(0)).getNodeValue().trim());
+        		
+        		NodeList imageList = firstPerksElement.getElementsByTagName(KEY_NAME_IMAGE);
+        		Element firstImageElement = (Element)imageList.item(0);
+        		NodeList textImageList = firstImageElement.getChildNodes();
+        		map.put(KEY_NAME_IMAGE, ((Node)textImageList.item(0)).getNodeValue().trim());
+        		
+        		perksListCollection.add(map);
+        	}
         }
+        PerksAdapter adapter = new PerksAdapter(this, perksListCollection);
         
         list = (ListView)findViewById(R.id.list);
         
-        adapter = new PerksAdapter(this, companyList);
         list.setAdapter(adapter);
         
         list.setOnItemClickListener(new OnItemClickListener() {
