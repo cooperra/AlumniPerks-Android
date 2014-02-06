840f7a3da6e23483d3f4bc346dae5426981dfcc1
Robbie Cooper <cooperra@rose-hulman.edu>
2014-02-05 22:40:59 -0500

    App now remembers whether you are logged in

---
+++ RosePerks/src/edu/rosehulman/roseperks/AutoLoginActivity.java
@@ -0,0 +1,21 @@
+package edu.rosehulman.roseperks;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class AutoLoginActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		String sessionkey = CredentialManager.getSessionKey(getApplicationContext());
+		if ("".equals(sessionkey)) {
+			Intent DetailView = new Intent(getApplicationContext(), LoginActivity.class);
+			startActivity(DetailView);
+		} else {
+			Intent DetailView = new Intent(getApplicationContext(), MainScreenActivity.class);
+			startActivity(DetailView);
+		}
+	}
+}
