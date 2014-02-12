76e9eeb520f07f133e368e08eeffae8bdd9b300d
Thomas Morris <morrista@rose-hulman.edu>
2014-02-12 14:51:00 -0500

    Button works

--- RosePerks/src/edu/rosehulman/roseperks/CategoryListActivity.java
+++ RosePerks/src/edu/rosehulman/roseperks/CategoryListActivity.java
@@ -1,15 +1,90 @@
 package edu.rosehulman.roseperks;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class CategoryListActivity extends Activity {
+	private ImageButton Restaurant;
+	private ImageButton Entertainment;
+	private ImageButton Hotel;
+	private ImageButton Bars;
+	private ImageButton Stores;
+	private ImageButton Other;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_categories);
+		Restaurant = (ImageButton) findViewById(R.id.restaurant_cat);
+		Entertainment = (ImageButton) findViewById(R.id.entertainment_cat);
+		Hotel = (ImageButton) findViewById(R.id.hotel_cat);
+		Bars = (ImageButton) findViewById(R.id.bars_cat);
+		Stores = (ImageButton) findViewById(R.id.stores_cat);
+		Other = (ImageButton) findViewById(R.id.other_cat);
 		
+		Restaurant.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent restaurantView = new Intent(getApplicationContext(),
+						RestaurantListView.class);
+				startActivity(restaurantView);
+				
+			}
+		});
+		
+		Entertainment.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent entertainmentView = new Intent(getApplicationContext(),
+						EntertainmentListView.class);
+				startActivity(entertainmentView);
+				
+			}
+		});
+		
+		Hotel.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		Bars.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		Stores.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		Other.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 
 	@Override
